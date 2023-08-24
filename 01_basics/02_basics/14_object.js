04f38e201dd8223a0945eaf49447d6314eaa81ed
Harshita-arora123 <142654786+Harshita-arora123@users.noreply.github.com>
2023-08-24 04:46:39 +0000

    done

---
+++ 01_basics/02_basics/14_object.js
@@ -0,0 +1,8 @@
+//const tinderUser = new Object()           {}  return empty {}
+const tinderUser={}           //   also return empty {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser); 
