fa05bab475a26804ecb92ec5c06320240681f1aa
Harshita-arora123 <142654786+Harshita-arora123@users.noreply.github.com>
2023-08-24 11:00:03 +0000

    okie

--- 01_basics/14_object.js
+++ 01_basics/14_object.js
@@ -30,4 +30,58 @@ const obj4 = {5:"a",6:"b"}
 const obj3 ={...obj1, ...obj2}
 console.log(obj3);         
 
+// database value how to use in object
+const users = [
+    {
+    },
+    { 
+    },
+    {
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));     // .keys mtlb kis obj pe method run     keys jo left side me h 
+console.log(Object.values(tinderUser));   //  .values right side    
+console.log(Object.entries(tinderUser));    // entries   both side   & all are print in arrays
+
+// .hasOwnProperty Determines whether an object has a property with the specified name.
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true 
+console.log(tinderUser.hasOwnProperty('isLogged'));    // false
+
+
+// de structuring the value of objects & arrays
+
+const course={
+    coursename: "js in hindi",
+    price : "999",
+    courseInstructor: "hani"
+
+}
+// course.courseInstructor
+const {courseInstructor: instructor}=course    // :instructor likh k chote nam se b access kra skte h
+//console.log(courseInstructor);
+console.log(instructor);   // we can write this 
+
+
+// json  works  both object{} & api []
+// api  earlier value r in d form of xml ,later value r in d form of json
+// json syntax  both values & keys are string
+// json to api call?
+
+/*{ 
+    "name":  "harshita",                         json syntax without object name
+    "coursename" :"js in hindi",
+    "price": "free",
+} */
+
+[
+    {},
+    {},
+    {}
+    
+    
+]
 
